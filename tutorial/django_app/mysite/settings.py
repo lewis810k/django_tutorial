4a765939b7c1ed405cf03c9375ec8abf29169177
lewis810k <alsdl901@gmail.com>
2017-02-06 17:41:07 +0900

    print Question list from index view

--- tutorial/django_app/mysite/settings.py
+++ tutorial/django_app/mysite/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# 템플릿 디렉토리
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -61,7 +63,9 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATES_DIR,
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- tutorial/django_app/polls/views.py
+++ tutorial/django_app/polls/views.py
@@ -1,8 +1,36 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # pub_date 칼럼(필드)를 기준으로 내림차순 정렬한 결과를 latest_question_list에 할당
+    latest_question_list = Question.objects.order_by('-pub_date')
 
+    # 템플릿 로더를 이용해서 'polls/index.html'에서 템플릿파일을 가져온다
+    template = loader.get_template('polls/index.html')
+
+    # 템플릿 파일에 전달할 context 객체를 정의
+    context = {
+        # latest_question_list 라는 키에 값을 할당, 해당 키로 템플릿에서 사용가능하다.
+        'latest_question_list': latest_question_list,
+    }
+    # 템플릿에 context와 request 객체를 사용해서 render해준 결과를 돌려줌
+    return HttpResponse(template.render(context, request))
+
+
+    # # 돌려줄 문자열을 작성
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    #
+    # output2 = ''
+    #
+    # # 마지막 콤마를 빼기 위해서 enumerate로 활용할 수 있다
+    # for q in latest_question_list:
+    #     output2 += q.question_text + ', '
+    # output2 = output2[:-2]
+    #
+    # return HttpResponse(output)
+    #
 def detail(request, question_id):
     return HttpResponse("You're looking at question {}".format(question_id))
 
